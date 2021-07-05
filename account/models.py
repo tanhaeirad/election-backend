808deaa2ec4d1b628581a7da014774d1fdc32ea2
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-05 17:28:26 +0430

    create admin, inspector, supervisor and user models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+# TODO: zone should be change and refer to zone model
 
-# Create your models here.
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class User(AbstractUser):
+    class UserKind(models.TextChoices):
+        VISITOR = 'Visitor', _('Visitor')
+        INSPECTOR = 'Inspector', _('Inspector')
+        SUPERVISOR = 'Supervisor', _('Supervisor')
+        ADMIN = 'Admin', _('Admin')
+
+    kind = models.CharField(max_length=32, choices=UserKind.choices, default=UserKind.VISITOR)
+
+    # def _is_inspector(self):
+    #     try:
+    #         obj = Inspector.objects.get(pk=self.pk)
+    #
+    #     except Inspector.DoesNotExist:
+    #         obj = None
+    #
+    #     finally:
+    #         return bool(obj)
+    #
+    # def _is_supervisor(self):
+    #     try:
+    #         obj = Supervisor.objects.get(pk=self.pk)
+    #
+    #     except Supervisor.DoesNotExist:
+    #         obj = None
+    #
+    #     finally:
+    #         return bool(obj)
+    #
+    # def _is_admin(self):
+    #     try:
+    #         obj = Admin.objects.get(pk=self.pk)
+    #
+    #     except Admin.DoesNotExist:
+    #         obj = None
+    #
+    #     finally:
+    #         return bool(obj)
+
+
+class Inspector(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    zone = models.IntegerField()
+
+
+class Supervisor(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    zone = models.IntegerField()
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
