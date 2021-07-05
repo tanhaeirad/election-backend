7aabd1f33c3d6ef70bf661ef94dbb1eb7904e446
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-05 17:27:22 +0430

    add test file to account app

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
+from django.utils.encoding import force_text
+from rest_framework import status
 
-# Create your tests here.
+from .models import User, Inspector, Supervisor, Admin
+
+# Define CONSTANTS
+LOGIN_ENDPOINT = '/account/login/'
+REGISTER_ENDPOINT = '/account/register/'
+GET_ALL_USERS_ENDPOINT = '/account/users/'
+
+
+class AccountTest(TestCase):
+    def test_login(self):
+        User.objects.create_user('test_user', 'something@gmail.com', '123')
+
+        # good login
+        data = {'username': 'test_user', 'password': '123'}
+        response = self.client.post(LOGIN_ENDPOINT, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # bad login
+        data = {'username': 'admin', 'password': '321'}
+        response = self.client.post(LOGIN_ENDPOINT, data=data)
+        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_register(self):
+        # good register
+        data = {'username': 'user1', 'password': '123', 'password2': '123'}
+        response = self.client.post(REGISTER_ENDPOINT, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # bad register - username is exist
+        data = {'username': 'user1', 'password': '321', 'password2': '321'}
+        response = self.client.post(REGISTER_ENDPOINT, data=data)
+        self.assertNotEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # bad register - password and password2 not the same
+        data = {'username': 'user2', 'password': '123', 'password2': '321'}
+        response = self.client.post(REGISTER_ENDPOINT, data=data)
+        self.assertNotEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_all_user(self):
+        # create user1 visitor
+        user1 = User.objects.create_user(username='user1', password='123')
+
+        # create user2 inspector
+        user2 = User.objects.create_user(username='user2', password='123')
+        Inspector.objects.create(user=user2, zone=31)
+
+        # create user3 supervisor
+        user3 = User.objects.create_user(username='user3', password='123')
+        Supervisor.objects.create(user=user3, zone=31)
+
+        # create user4 admin
+        user4 = User.objects.create_user(username='user4', password='123')
+        Admin.objects.create(user=user4)
+
+        response = self.client.get(GET_ALL_USERS_ENDPOINT)
+
+        raw = force_text(response.content)
+        excepted_data = [
+            {
+                'username': 'user1',
+                'kind': 'Visitor'
+            },
+            {
+                'username': 'user2',
+                'kind': 'Inspector'
+            },
+            {
+                'username': 'user3',
+                'kind': 'Supervisor'
+            },
+            {
+                'username': 'user4',
+                'kind': 'Admin'
+            }
+        ]
+        self.assertJSONEqual(raw, excepted_data)
