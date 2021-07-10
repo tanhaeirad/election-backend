efd925ba0f57c5797506db7fac685e0d2e970cba
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-11 03:28:44 +0430

    add CityViewSet

--- election/views.py
+++ election/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import City
+from .serializers import CitySerializer
+
+
+class CityViewSet(ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+    # TODO: should add permission classes
+    # permission_classes = ...
+
+
