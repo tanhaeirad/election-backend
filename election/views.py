c48f8bb3cf72dd9eb881c677ffd17885fca243e2
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-11 05:29:56 +0430

    create ZoneViewSet and ZonesOfCityAPIView

--- election/views.py
+++ election/views.py
@@ -1,7 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from .models import City
-from .serializers import CitySerializer
+from .models import City, Zone
+from .serializers import CitySerializer, ZoneSerializer
 
 
 class CityViewSet(ModelViewSet):
@@ -12,3 +14,16 @@ class CityViewSet(ModelViewSet):
     # permission_classes = ...
 
 
+class ZoneViewSet(ModelViewSet):
+    queryset = Zone.objects.all()
+    serializer_class = ZoneSerializer
+
+    # TODO: should add permission classes
+    # permission_classes = ...
+
+
+class ZonesOfCityAPIView(APIView):
+    def get(self, request, city_id):
+        zones = Zone.objects.filter(city=city_id)
+        data = [{'id': zone.id, 'name': zone.name, 'city': zone.city.id} for zone in zones]
+        return Response(data)
