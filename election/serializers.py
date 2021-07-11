757974806d0e48376bb596d7754881dec2691e8b
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-11 05:29:36 +0430

    add ZoneSerializer

--- election/serializers.py
+++ election/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import City
+from .models import City, Zone
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
+        fields = '__all__'
+
+
+class ZoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Zone
         fields = '__all__'
