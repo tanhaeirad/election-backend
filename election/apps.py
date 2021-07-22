6c8473eb9d1e0bd32683d2f754807c7c6561db1c
tanhaeirad <amirrezatanhaei@yahoo.com>
2021-07-22 11:49:46 +0430

    delete signals for election app

--- election/apps.py
+++ election/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class ElectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'election'
-
-    def ready(self):
-        import election.signals
